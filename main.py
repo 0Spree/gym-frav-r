81248ae0930efe78d60d000c7bd314b1f3097044
Spree <andreas.jakobsen889@live.dk>
2022-12-06 20:40:02 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -8,21 +8,20 @@ if st.selectbox('Vil du regne for: ',['Perioden', 'Året']) == 'Perioden':
     maxp = int(10)
     moduler = float(st.number_input(label="Hvor mange moduler har du i perioden?",step=1.,format="%.2f", min_value=0., max_value=10000.))
     missingmoduler = float(st.number_input("Hvor mange moduler har du været fraværende? ",step=1.,format="%.2f", min_value=0., max_value=10000.))
-
-    try:
-        udregning = round(procent/moduler * missingmoduler, 5)
-    except ZeroDivisionError:
-        udregning = 0
-    
-    st.text("Du har " + str(udregning) + "% fravær")
-    graf = maxp-udregning
-    if float(graf) <= 10.0:
-        st.text("Du har " + str(graf) + "% fravær tilbage ud af de 10% tilladte")
-    elif float(graf) < 0:
-        st.text("Du har for meget")
-
+    if moduler < missingmoduler:
+        st.text("Du kan ikke have flere fraværende moduler end moduler")
     else:
-        st.text("Idk")
+        try:
+            udregning = round(procent/moduler * missingmoduler, 5)
+        except ZeroDivisionError:
+            udregning = 0
+        
+        st.text("Du har " + str(udregning) + "% fravær")
+        graf = maxp-udregning
+        if float(graf) < -0.000001:
+            st.text("Du har for meget")
+        else:
+            st.text("Du har " + str(graf) + "% fravær tilbage ud af de 10% tilladte")
 
 
 else:
@@ -30,18 +29,17 @@ else:
     maxp2 = 10
     moduler2 = float(st.number_input(label="Hvor mange moduler har du for året?",step=1.,format="%.2f", min_value=0., max_value=10000.))
     missingmoduler2 = float(st.number_input("Hvor mange moduler har du været fraværende? ",step=1.,format="%.2f", min_value=0., max_value=10000.))
-
-    try:
-        udregning2 = round(procent2/moduler2 * missingmoduler2, 5)
-    except ZeroDivisionError:
-        udregning2 = 0
-    
-    st.text("Du har " + str(udregning2) + "% fravær")
-    graf = maxp2-udregning2
-    if float(graf) <= 10.0:
-        st.text("Du har " + str(graf) + "% fravær tilbage ud af de 10% tilladte")
-    elif float(graf) < 0:
-        st.text("Du har for meget")
-
+    if moduler2 < missingmoduler2:
+        st.text("Du kan ikke have flere fraværende moduler end moduler")
     else:
-        st.text("Idk")
+        try:
+            udregning2 = round(procent2/moduler2 * missingmoduler2, 5)
+        except ZeroDivisionError:
+            udregning2 = 0
+        
+        st.text("Du har " + str(udregning2) + "% fravær")
+        graf = maxp2-udregning2
+        if float(graf) < -0.000001:
+            st.text("Du har for meget")
+        else:
+            st.text("Du har " + str(graf) + "% fravær tilbage ud af de 10% tilladte")
